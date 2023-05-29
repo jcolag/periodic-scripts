3a781b325e7d2783fbe7c958f5576f39e5159d11
John Colagioia <john@colagioia.net>
2023-05-29 06:48:00 -0400

    Refactor to use desktop shortcuts

--- editor.sh
+++ editor.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 tempfile=$(mktemp --suffix=.txt)
 echo "Time to wake up!" > "$tempfile"
-editor=$(mimetype -b "$tempfile" | xargs xdg-mime query default | xargs whereis -b | cut -f2 -d' ')
+handler=$(mimetype -b "$tempfile" | xargs xdg-mime query default)
+editor=
+
+if echo "$handler" | grep -q ".desktop$" > /dev/null
+then
+  editor=$(grep -w Exec "/usr/share/applications/$handler" | head -1 | cut -f2 -d'=' | cut -f1 -d' ' | xargs whereis -b | cut -f2 -d' ')
+else
+  editor=$(whereis -b "$handler" | cut -f2 -d' ')
+fi
 
 for file in "$@"
 do

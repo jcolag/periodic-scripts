9643f4d6961df4e70d663b75e81f10e8dea3c5a9
John Colagioia <john@colagioia.net>
2023-10-30 06:50:45 -0400

    Update light/dark mode for current computer

--- night.sh
+++ night.sh
@@ -2,22 +2,31 @@
 theme='solarized-light'
 nlight=false
 scheme=prefer-light
+pop=Pop-light
+redshift=$(pgrep --exact redshift)
 
 if [ "$#" -ge 1 ]
 then
   theme='solarized-dark'
   nlight=true
   scheme=prefer-dark
+  pop=Pop-dark
 fi
 
-gsettings set org.gnome.gedit.preferences.editor scheme \
-  "'${theme}'"
+# Note that you probably either want Night-light set here OR Redshift toggled
+# below, not both.
 gsettings set org.gnome.settings-daemon.plugins.color \
   night-light-enabled "${nlight}"
-sleep 15
+sleep 10
 gsettings set org.gnome.desktop.interface color-scheme "${scheme}"
+sleep 5
+gsettings set org.gnome.desktop.interface gtk-theme "${pop}"
 sleep 5
 gnome-control-center &
 sleep 10
 pkill --exact gnome-control-c
+gsettings set org.gnome.gedit.preferences.editor scheme \
+  "'${theme}'"
+sleep 5
+kill -USR1 "${redshift}"
 
